ad5b5852c250a4314a6fe21507edd46e77aafe2b
Utilisateur <user@debian.arcahe.ovh>
2019-10-23 12:01:28 +0200

    Maj de MoteurRPN

--- src/MoteurRPN.java
+++ src/MoteurRPN.java
@@ -12,6 +12,8 @@ public class MoteurRPN {
 	
 	private static void calculate (Operation op) throws DivisionParZeroException, OperandeManquanteException {
 		
+		double res=0;
+		
 		if (Pile.size()== 1 || Pile.isEmpty()) {
 	    	//System.out.println("llllll");
 	    	throw new OperandeManquanteException();
@@ -23,20 +25,42 @@ public class MoteurRPN {
 	    	//System.out.println("cccccccc");
 	    	throw new DivisionParZeroException();
 	    }
+		
+		
+//		if (op.symbol == '+') {
+//			res = op.PLUS.eval(top2, top1);
+//		}
+		
+		switch (op.symbol) {
+		case '/' : res = op.DIV.eval(top2, top1); break;
+		
+		case '+' : res = op.PLUS.eval(top2, top1); break;
+		
+		case '*' : res = op.MULT.eval(top2, top1); break;
+		
+		case '-' : res = op.MOINS.eval(top2, top1); break;
+		}
+		
+		ajouterAPile(res);
+		
+		System.out.println("the result is : " + res);
 
 	}
 	
 	public static void main (String s[]) throws DivisionParZeroException, OperandeManquanteException{
-    	Operation op = Operation.DIV;
+    	Operation op = Operation.MOINS;
+    	Operation op2 = Operation.MULT;
 //		double a,b;
 //		a= 10; b = 5;
 //		a = op.eval(a, b);
 //		System.out.println(a);
-		//ajouterAPile(5);
+//		ajouterAPile(5);
 		ajouterAPile(9);
+		ajouterAPile(6);
 		
 		calculate (op);
 		
-	}
-
-}
+		ajouterAPile(4);
+		
+		calculate (op2);
+		
